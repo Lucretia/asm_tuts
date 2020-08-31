087bf5fef60c5da9c1b9a2e33179b3775e864aa8
Luke A. Guest <laguest@archeia.com>
2020-08-31 10:48:01 +0100

    Add bit testing example. Add "homework" for teq instruction.

---
+++ derek_banas/tutorial_2/part_3/asmtut.s
@@ -0,0 +1,18 @@
+.global _start
+
+_start:
+	mov r1, #9	@ 1001
+	mov r2, #8	@ 1000
+	@ tst r1, r2	@ r1 - r2
+	teq r1, r2
+	beq _bit_set
+	mov r0, #1
+	b end
+
+_bit_set:
+	mov r0, #0
+
+end:
+	mov r7, #1
+	swi 0
+
